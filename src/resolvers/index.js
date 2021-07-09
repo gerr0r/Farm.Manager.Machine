cd85b2d779e30b8b9e9ee67d625460cda79e659c
Gergan Ruschev <gergan.ruschev@yara.com>
2021-07-09 12:07:26 +0300

    feat/Type defs and resolvers updates

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -3,6 +3,12 @@ const { hasRights } = require('../helpers/auth')
 
 module.exports = {
     Query: {
+        async getMachine(parent, args, context) {
+            const { token } = context
+            if (!hasRights(token, 'admin')) throw new Error('Unauthorized')
+            return await db.Machine.findByPk(args.id)
+        },
+
         async getMachines(parent, args, context) {
             const { token } = context
             if (!hasRights(token, 'master')) throw new Error('Unauthorized')
--- src/type-definitions/index.js
+++ src/type-definitions/index.js
@@ -2,11 +2,14 @@ const { gql } = require('apollo-server')
 
 module.exports = gql`
   extend type Query {
+    getMachine(id: ID!): Machine
     getMachines: [Machine]
   }
 
   type Machine @key(fields: "id") {
     id: ID!
     model: String!
+    engine: String
+    type: String
   }
 `
